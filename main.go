be3c14cf909c476377cde48c2c57fd9f2d463248
Glenn <phoobynet@users.noreply.github.com>
2022-10-18 11:02:59 +0100

    frontend

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/phoobynet/sec-financial-statements/companies"
 	"github.com/phoobynet/sec-financial-statements/database"
 	"github.com/phoobynet/sec-financial-statements/quarterly"
@@ -18,6 +17,11 @@ func main() {
 	outPath := flag.String("o", "", "The output directory")
 	flag.Parse()
 
+	if *outPath == "" {
+		flag.PrintDefaults()
+		return
+	}
+
 	zipFileStat, zipFileStatErr := os.Stat(*sourceZip)
 
 	if os.IsNotExist(zipFileStatErr) {
@@ -25,24 +29,21 @@ func main() {
 		log.Fatalln("Source zip file does not exist")
 	}
 
-	if *outPath == "" {
-		flag.PrintDefaults()
-		return
-	}
-
 	zipFileStat.Name()
 
 	dbPath := filepath.Join(*outPath, strings.TrimSuffix(zipFileStat.Name(), ".zip")+".db")
 
-	if _, err := os.Stat(dbPath); !os.IsNotExist(err) {
-		log.Fatalln("Database already exists")
+	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
+		log.Printf("Database path: %s\n", dbPath)
+
+		log.Printf("Loading database (this may take a while)...")
+
+		db := database.Init(dbPath)
+		sics.Load(db)
+		companies.Load(db)
+		quarterly.Load(db, *sourceZip)
+		database.CreateIndexes(db)
+	} else {
+		log.Printf("Database already exists at %s...starting server", dbPath)
 	}
-
-	fmt.Printf("Database path: %s\n", dbPath)
-
-	db := database.Init(dbPath)
-	sics.Load(db)
-	companies.Load(db)
-	quarterly.Load(db, *sourceZip)
-	database.CreateIndexes(db)
 }
